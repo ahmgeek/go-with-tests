69306cedd297911143f98af984202dd73c6830f6
Ahmed Tolba <tolpa1@gmail.com>
2018-04-29 20:09:21 +0200

    Rename directory

---
+++ strng_playground/strng_test.go
@@ -0,0 +1,12 @@
+package strng
+
+import "testing"
+
+func TestContain(t *testing.T){
+	result := Contain("seafood", "foo")
+	expected := true
+
+	if result != true {
+		t.Errorf("expected '%t' got '%t'", expected, result)
+	}
+}
