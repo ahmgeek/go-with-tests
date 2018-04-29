a7d7d2fca8f128e2e0a18217bb309ee9ef772555
Ahmed Tolba <tolpa1@gmail.com>
2018-04-29 20:10:25 +0200

    Remove duplicates

--- srng_playground/strng.go
+++
@@ -1,9 +0,0 @@
-package strng
-
-import "strings"
-
-// Contain method; tests strings.Contains from "strings" package,
-// just for fun.
-func Contain(word, target string) bool {
-	return strings.Contains(word, target)
-}
