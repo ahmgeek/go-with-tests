0d17b8d4881568b585df039c1c06d116c7401a3c
Ahmed Tolba <tolpa1@gmail.com>
2018-04-29 21:13:49 +0200

    Dry tests and add more tests for ContainsAny

--- strng_playground/strng.go
+++ strng_playground/strng.go
@@ -2,8 +2,14 @@ package strng
 
 import "strings"
 
-// Contain method; tests strings.Contains from "strings" package,
+// Contains method; tests strings.Contains from "strings" package,
 // just for fun.
-func Contain(word, target string) bool {
-	return strings.Contains(word, target)
+func Contains(str, substr string) bool {
+	return strings.Contains(str, substr)
+}
+
+// ContainsAny method; tests strings.ContainsAny from "strings" package,
+// Just for meh.
+func ContainsAny(str, substr string) bool {
+	return strings.ContainsAny(str, substr)
 }
--- strng_playground/strng_test.go
+++ strng_playground/strng_test.go
@@ -2,11 +2,49 @@ package strng
 
 import "testing"
 
-func TestContain(t *testing.T){
-	result := Contain("seafood", "foo")
-	expected := true
-
-	if result != true {
+var assertCorrectMessage = func(t *testing.T, expected, result bool) {
+	t.Helper()
+	if result != expected {
 		t.Errorf("expected '%t' got '%t'", expected, result)
 	}
+}
+
+func TestContains(t *testing.T){
+	t.Run("Contains substring", func(t *testing.T) {
+		result := Contains("seafood", "foo")
+		expected := true
+
+		assertCorrectMessage(t, expected, result)
+	})
+
+	t.Run("Doesn't Contain substring", func(t *testing.T) {
+		result := Contains("seafood", "achmed")
+		expected := false
+
+		assertCorrectMessage(t, expected, result)
+	})
+}
+
+func TestContainsAny(t *testing.T){
+	t.Run("Contains a char", func(t *testing.T) {
+		result := ContainsAny("seafood", "s")
+		expected := true
+
+		assertCorrectMessage(t, expected, result)
+	})
+
+	t.Run("Contains multiple chars", func(t *testing.T) {
+		result := ContainsAny("seafood", "d & foo & sea")
+		expected := true
+
+		assertCorrectMessage(t, expected, result)
+	})
+	
+
+	t.Run("Doesn't Contain substring", func(t *testing.T) {
+		result := ContainsAny("seafood", "g")
+		expected := false
+
+		assertCorrectMessage(t, expected, result)
+	})
 }
