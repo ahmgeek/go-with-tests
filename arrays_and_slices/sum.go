977f1345c04155f1cb00ff16bf09574734311584
Ahmed Tolba <tolpa1@gmail.com>
2018-05-28 21:12:24 +0200

    Add make

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,9 +1,23 @@
 package sum
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numberToSum ...[]int) []int {
+	length := len(numberToSum)
+	sums := make([]int, length)
+
+	for key, value := range numberToSum {
+		sums[key] = Sum(value)
+		fmt.Print("-------------------------\n")
+		fmt.Printf("%d \n",key)
+	}
+	return sums
 }
