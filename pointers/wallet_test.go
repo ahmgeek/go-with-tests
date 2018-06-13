26a0305b493cb83ceaac923d2b715b24fd85d086
Ahmed Tolba <tolpa1@gmail.com>
2018-06-13 15:39:49 +0200

    Add error handling

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -30,4 +30,17 @@ func TestWallet(t *testing.T) {
 		asserBalance(t, wallet, Bitcoin(10))
 	})
 	
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		asserBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	})
 }
