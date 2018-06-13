9dc55f587fddc32bb7c71a04f691868f6ab12199
Ahmed Tolba <tolpa1@gmail.com>
2018-06-13 14:50:44 +0200

    Add stringer format to type alias

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,10 +1,19 @@
 package wallet
 
+import "fmt"
 
 type Bitcoin int
 
+type Stringer interface {
+	String() string
+}
+
 type Wallet struct {
 	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
